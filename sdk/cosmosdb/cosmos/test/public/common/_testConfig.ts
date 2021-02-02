42c72d451db337f4dd6fc3de1c2294a13cc54d18
zfoster <notzachfoster@gmail.com>
2021-02-02 11:18:47 -0500

    format

--- sdk/cosmosdb/cosmos/test/public/common/_testConfig.ts
+++ sdk/cosmosdb/cosmos/test/public/common/_testConfig.ts
@@ -3,7 +3,9 @@
 // [SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine")]
 const masterKey =
   "2824kkTRDtkn5fWVotPZjett8oPfXou2PH2rTZmrumICdVT5eSItsfBadkRhk5OyoDHt5KodqqImtBaatiAQUw==";
-const endpoint = 'https://sqloncomputejsstagesignoff.documents-staging.windows-ppe.net:443/' || "https://localhost:8081";
+const endpoint =
+  "https://sqloncomputejsstagesignoff.documents-staging.windows-ppe.net:443/" ||
+  "https://localhost:8081";
 
 // This is needed to disable SSL verification for the tests running against emulator.
 if (endpoint.includes("https://localhost")) {
