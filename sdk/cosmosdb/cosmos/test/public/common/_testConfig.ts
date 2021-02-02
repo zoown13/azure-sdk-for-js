3e713ccb2536fe736fa936d4563cbfa2db90fcaf
zfoster <notzachfoster@gmail.com>
2021-02-02 11:46:19 -0500

    Adds json console log and handler

--- sdk/cosmosdb/cosmos/test/public/common/_testConfig.ts
+++ sdk/cosmosdb/cosmos/test/public/common/_testConfig.ts
@@ -2,10 +2,9 @@
 // Licensed under the MIT license.
 // [SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine")]
 const masterKey =
-  "2824kkTRDtkn5fWVotPZjett8oPfXou2PH2rTZmrumICdVT5eSItsfBadkRhk5OyoDHt5KodqqImtBaatiAQUw==";
-const endpoint =
-  "https://sqloncomputejsstagesignoff.documents-staging.windows-ppe.net:443/" ||
-  "https://localhost:8081";
+  process.env.ACCOUNT_KEY ||
+  "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+const endpoint = process.env.ACCOUNT_HOST || "https://localhost:8081";
 
 // This is needed to disable SSL verification for the tests running against emulator.
 if (endpoint.includes("https://localhost")) {
